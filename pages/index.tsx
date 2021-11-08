98caf9f4f618f3bf2c827c2fb6fca45bdc6534b5
suhyeonP <ahah12k@gmail.com>
2021-11-08 09:08:25 +0900

    :fire: remove yarn3

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,6 @@
 import React from 'react';
-import { useQuery } from 'react-query';
 
 const Home = (): JSX.Element => {
-  const { isLoading, error, data, isFetching } = useQuery('repoData', () =>
-    fetch('https://api.github.com/repos/tannerlinsley/react-query').then(res =>
-      res.json()
-    )
-  );
-  console.log(data);
   return <div>home</div>;
 };
 
